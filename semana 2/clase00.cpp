da974f003fdf74d6fe5c0bac8438dcc31b9c2420
rivero9 <106290605+rivero9@users.noreply.github.com>
2024-06-03 15:43:41 -0400

    Semana 2.1

---
+++ semana 2/clase00.cpp
@@ -0,0 +1,34 @@
+// clase00.cpp
+# include <iostream>
+using namespace std;
+
+class Gato { // Inicializamos la clase "Gato"                
+public:   // Declaramos que las siguientes serán en modo "public"
+    int ObtenerEdad();    // le pasamos la funcion ObtenerEdad a la clase
+    void AsignarEdad(int edad);   // le pasamos la funcion AsignarEdad a la clase
+    void Maullar();  // le pasamos la funcion Maullar a la clase
+private:    // declaramos que la siguientes variables serán privadas, es decir que no se puede editar por otro programador
+    int suEdad;    // declaramos la variable edad
+};
+
+int Gato::ObtenerEdad() {    // creamos una función para la clase Gato, la cual devuelve la edad
+    return suEdad;
+}
+
+void Gato::AsignarEdad(int edad) {      // creamos una función para la clase Gato,  que recibe un parámetro para darle una edad a Gato
+    suEdad = edad;
+
+}
+void Gato::Maullar() {    // creamos una función para la clase Gato, que muestra un mensaje simulando un maullido
+    cout << "Miau!\n";
+}
+
+int main() {
+    Gato Pelusa;    // creamos un objeto llamado Pelusa, usando la clase Gato
+    Pelusa.AsignarEdad(5);   // le asignamos una edad al objeto
+    Pelusa.Maullar();   // ejecutamos la funcion Maullar del objeto para mostrar un mensaje en la pantalla
+    cout << "Pelusa es un gato que tiene ";    // por último mostramos en pantalla cuantos años de edad tiene el objeto, obteniendo la edad con la funcion ObtenerEdad()
+    cout << Pelusa.ObtenerEdad() << " años de edad.\n";
+    Pelusa.Maullar();    // mostramos el mensaje del maullido nuevamente 
+    return 0;
+}
---
+++ semana 2/clase01.cpp
@@ -0,0 +1,51 @@
+# include <iostream>
+
+
+// este código es bastante parecido al anterior a diferencia de lo siguiente: 
+
+using namespace std;
+class Gato {
+public:
+    Gato (int edadInicial);
+    ~Gato();
+    int ObtenerEdad();
+    void AsignarEdad(int edad);
+    void Maullar();
+
+private:
+    int suEdad;
+};
+
+
+Gato::Gato(int edadInicial) {
+    cout << "Ingresando al constructor...\n";
+    suEdad=edadInicial;
+}
+
+Gato::~Gato() {
+    cout << "Ingresando al destructor...\n";
+}
+
+int Gato::ObtenerEdad() {
+    return suEdad;
+}
+
+void Gato::AsignarEdad(int edad) {
+    suEdad = edad;
+}
+
+void Gato::Maullar() {
+    cout << "Miau!\n";
+}
+
+int main() {
+    Gato Pelusa(5);  // cuando creamos el objetos le pasamos un numero, el cual va a hacer la edad inicial
+    Pelusa.Maullar();
+    cout << "Pelusa es un gato que tiene ";
+    cout << Pelusa.ObtenerEdad() << " años de edad.\n";
+    Pelusa.Maullar();
+    Pelusa.AsignarEdad(7);   // y luego lo podemos reasignar la edad
+    cout << " Ahora Pelusa tiene ";
+    cout << Pelusa.ObtenerEdad() << " años de edad.\n";
+    return 0;
+}
